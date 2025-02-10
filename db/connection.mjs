2297415bc9fa83c4f3ef3fc6658aae7d0a868c16
codin <douglasd3v.contato@gmail.com>
2025-02-10 16:23:08 -0300

    Alteração nas configurações de conexão do banco de dados, adição de tratativa de erro.

--- db/connection.mjs
+++ db/connection.mjs
@@ -1,9 +1,20 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }
+);
+
+try {
+  sequelize.authenticate();
+  console.log('Conectado ao banco de dados com sucesso.');
+} catch (error) {
+  console.log(`Erro ao conectar ao banco de dados: ${error}`);
+}
 
 export default sequelize;
