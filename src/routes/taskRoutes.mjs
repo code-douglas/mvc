83490650fec4a247017d468d1057342aa542f773
codin <douglasd3v.contato@gmail.com>
2025-02-10 20:51:15 -0300

    Adicionando rota de POST para criação de TASK

--- src/routes/taskRoutes.mjs
+++ src/routes/taskRoutes.mjs
@@ -5,6 +5,7 @@ const router = express.Router();
 import TaskController from '../controllers/TaskController.mjs';
 
 router.get('/add', TaskController.createTask);
+router.post('/add', TaskController.createTaskSave);
 router.get('/', TaskController.showTasks);
 
 export default router;
