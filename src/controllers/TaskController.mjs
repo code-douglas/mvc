59765ebe5e64de8168e97701a5c27374c1f7a4ca
codin <douglasd3v.contato@gmail.com>
2025-02-10 20:52:35 -0300

    Adição de POST e do método createTaskSave

--- src/controllers/TaskController.mjs
+++ src/controllers/TaskController.mjs
@@ -4,6 +4,19 @@ export default class TaskController {
   static createTask(req, res) {
     res.render('tasks/create');
   }
+  static async createTaskSave(req, res) {
+    const { title, description } = req.body;
+
+    const task = {
+      title,
+      description,
+      done: false,
+    };
+
+    await Task.create(task)
+      .then(res.redirect('/tasks'))
+      .catch(error => console.log(error));
+  }
   static showTasks(req, res) {
     res.render('tasks/all');
   }
