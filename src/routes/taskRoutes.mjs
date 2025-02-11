28c9d916a909b0d2155db648f5421633e0d6f7d8
codin <douglasd3v.contato@gmail.com>
2025-02-11 10:47:10 -0300

    Rotas para deletar e editar dados do banco

--- src/routes/taskRoutes.mjs
+++ src/routes/taskRoutes.mjs
@@ -6,6 +6,8 @@ import TaskController from '../controllers/TaskController.mjs';
 
 router.get('/add', TaskController.createTask);
 router.post('/add', TaskController.createTaskSave);
+router.post('/remove', TaskController.removeTask);
+router.get('/edit/:id', TaskController.updateTask);
 router.get('/', TaskController.showTasks);
 
 export default router;
