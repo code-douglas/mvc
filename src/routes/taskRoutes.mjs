1ff69c3e9dfdb946325839ea582d07957262d5c1
codin <douglasd3v.contato@gmail.com>
2025-02-12 09:52:54 -0300

    Rotas para mudar o status e editar tarefa

--- src/routes/taskRoutes.mjs
+++ src/routes/taskRoutes.mjs
@@ -8,6 +8,8 @@ router.get('/add', TaskController.createTask);
 router.post('/add', TaskController.createTaskSave);
 router.post('/remove', TaskController.removeTask);
 router.get('/edit/:id', TaskController.updateTask);
+router.post('/edit/:id', TaskController.updateTaskSave);
+router.post('/updatestatus', TaskController.toggleTaskStatus);
 router.get('/', TaskController.showTasks);
 
 export default router;
